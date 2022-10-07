67eb31a6fa5d0e9c2314bedd1f6124afcb6d5dc3
Jesier <jesiersantiago@gmail.com>
2022-10-07 10:29:02 -0500

    push

---
+++ main.js
@@ -0,0 +1,37 @@
+const getBean = () => {
+    console.log('find a beanie and put it on (if you have flowy hair)')
+}
+
+const getBoard = () => {
+    console.log('grab your skateboard')
+}
+
+const goOutside = () => {
+    console.log('walk outside and find flat ground or downward slope')
+}
+
+const placeBoard = () => {
+    console.log('put your skateboard on the ground')
+}
+
+const placeFoot = () => {
+    console.log('on the board, place your dominate foot behind the front screws')
+}
+
+const thenPush = () => {
+    console.log('with your foot that isnt on the board, push and balance on the board')
+
+}
+
+const enjoyRide = () => {
+    console.log('relax and feel the breez as your cruzin along')
+
+}
+
+getBean()
+getBoard()
+goOutside()
+placeBoard()
+placeFoot()
+thenPush()
+enjoyRide()
